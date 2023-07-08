264ec3c8570a82a950374a22bc242ffb22b765c9
jeetdevx <jeet.devx@gmail.com>
2023-07-08 22:48:06 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from customer import Customer
+
+### Calling the Customer class
+
+papa = Customer()  # create me an object of this class
+store = papa.connect_company()
+print(store.company_store())
